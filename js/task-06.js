3de26a662d71969b6932fc30030ea6c0efb89f81
Yuriy Dobryanskiy <dobryansky@i.ua>
2023-12-14 01:21:16 +0200

    task6

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector("#controls");
+const createButton = controls.querySelector("[data-create]");
+const destroyButton = controls.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  const boxesArray = [];
+  for (let i = 0; i < amount; i++) {
+    const width = 30 + i * 10;
+    const height = 30 + i * 10;
+    const color = getRandomHexColor();
+
+    const box = document.createElement("div");
+    box.style.width = width + "px";
+    box.style.height = height + "px";
+    box.style.backgroundColor = color;
+
+    boxesArray.push(box);
+  }
+
+  boxes.innerHTML = boxesArray.map(({ outerHTML }) => outerHTML).join("");
+  controls.querySelector("input").value = "";
+}
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+//Create button
+createButton.addEventListener("click", () => {
+  const amount = Number(controls.querySelector("input").value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+});
+//Destroy button
+destroyButton.addEventListener("click", destroyBoxes);
