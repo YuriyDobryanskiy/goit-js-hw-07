9c7a1e699c9f5786acfc462f8a31a28dc778434b
Yuriy Dobryanskiy <dobryansky@i.ua>
2023-12-14 00:42:12 +0200

    task5

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const color = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+
+  body.style.backgroundColor = newColor;
+  color.textContent = newColor;
+});
