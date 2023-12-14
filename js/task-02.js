ca06409e7089fe352b7d9b857eb970c6028d5f72
Yuriy Dobryanskiy <dobryansky@i.ua>
2023-12-14 21:04:27 +0200

    add css

--- js/task-02.js
+++ js/task-02.js
@@ -1,41 +1,41 @@
 const images = [
-  {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
-  {
-    url: 'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Elephant Beside on Baby Elephant',
-  },
-  {
-    url: 'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Blue Geeen and Orange Parrot',
-  },
-  {
-    url: 'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Zebras on Zebra',
-  },
+ {
+  url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  alt: 'White and Black Long Fur Cat',
+ },
+ {
+  url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+ },
+ {
+  url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  alt: 'Group of Horses Running',
+ },
+ {
+  url: 'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  alt: 'Elephant Beside on Baby Elephant',
+ },
+ {
+  url: 'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  alt: 'Blue Geeen and Orange Parrot',
+ },
+ {
+  url: 'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  alt: 'Zebras on Zebra',
+ },
 ];
 
 const gallery = document.querySelector('.gallery');
 const createLis = images.map(
-  ({ url, alt }) => `
+ ({ url, alt }) => `
   <li>
-    <img src="${url}" alt="${alt}" width="300">
+    <img src="${url}" alt="${alt}">
   </li>
 `
 );
 gallery.innerHTML = createLis.join('');
 
 gallery.setAttribute(
-  'style',
-  'list-style-type:none; display: flex; flex-direction: row; gap:10px;'
+ 'style',
+ 'list-style-type:none; display: flex; flex-direction: row;'
 );
